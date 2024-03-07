2ceefa2041dbd71eaf6dcb9f8bfafe808313f875
Richard Sustek <ennspassion@gmail.com>
2024-03-07 15:30:00 +0100

    release

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,6 +1,6 @@
 
 export const libMetadata = {
 	name: '@kontent-ai-consulting/migration-toolkit',
-    timestamp: 'Thu, 07 Mar 2024 08:33:05 GMT',
-    version: '0.1.5'
+    timestamp: 'Thu, 07 Mar 2024 14:29:52 GMT',
+    version: '0.1.6'
 };
